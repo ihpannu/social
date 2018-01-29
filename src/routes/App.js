a03e00f3322bfe84b2212b4ff6ca4eeb03c08a27
iharmanpannu <iharmanpannu@gmail.com>
2018-01-28 22:44:49 -0800

    Initial commit

---
+++ src/routes/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Home from "../Components/Home";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Home />
+      </div>
+    );
+  }
+}
+
+export default App;
