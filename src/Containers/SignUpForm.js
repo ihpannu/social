43dbba69b1dce3b0b9248fd7f7edb348a9abcad3
iharmanpannu <iharmanpannu@gmail.com>
2018-01-28 23:52:19 -0800

    added form

---
+++ src/Containers/SignUpForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class SignUpForm extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" value="email" placeholder="Email" />
+      </form>
+    );
+  }
+}
