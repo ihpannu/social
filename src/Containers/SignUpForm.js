73ac94d0be36b93219b94f5d5b343ea72aebb28b
iharmanpannu <iharmanpannu@gmail.com>
2018-01-29 00:07:53 -0800

    exported default class

--- src/Containers/SignUpForm.js
+++ src/Containers/SignUpForm.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 
-class SignUpForm extends Component {
+export default class SignUpForm extends Component {
   render() {
     return (
       <form>
         <input type="text" value="email" placeholder="Email" />
+        <label>Hello World</label>
       </form>
     );
   }
